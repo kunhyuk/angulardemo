a5db85e50207d3b44b1edee824da41c3a3bbf1f8
Kunhyuk <klee762@gatech.edu>
2023-05-25 10:07:43 -0400

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { demoComponent } from './demo.component';
+import { FormComponent } from './form/form.component'
+import { demoComponentForm } from './demo.form';
+import { demoComponentFooter } from './demo.footer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    demoComponent,
+    FormComponent,
+    demoComponentForm,
+    demoComponentFooter
   ],
   imports: [
     BrowserModule
